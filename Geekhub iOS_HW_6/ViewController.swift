01acbdde94c332033e90d891c89676bf9360c016
Pavel Bondar <ivizey@gmail.com>
2019-11-22 13:17:28 +0200

    Initial Commit

---
+++ Geekhub iOS_HW_6/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Geekhub iOS_HW_6
+//
+//  Created by Pavel Bondar on 22.11.2019.
+//  Copyright © 2019 Pavel Bondar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
